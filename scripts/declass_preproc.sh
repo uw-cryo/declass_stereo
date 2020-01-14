a42e024bcf16908ac8671e860f755ca9c525017d
David Shean <dshean@gmail.com>
2020-01-14 09:50:26 -0800

    Review processing scripts, cleanup, improve comments

--- scripts/declass_preproc.sh
+++ scripts/declass_preproc.sh
@@ -1,35 +1,42 @@
 #! /bin/bash
-# Script to generate initial image and optical bar cameras 
-#directory containing image subsets
+
+#Wrapper shell script for sample Hexagon (KH-9) image pre-processing
+#Shashank Bhushan, David Shean
+
+#Requires command-line utilities from the NASA Ames Stereo Pipeline
+
+#TODO: Improved file handling to identify unique image IDs from all scanned subscenes, and run in parallel
+
+#Input image directory (containg *a.tif, *b.tif etc.)
 img_dir=$1
-#this expects the input filenames to be the same as provided by USGS
-img_list=$(ls $img_dir/*.tif)
+#Input optical bar camera model template, in files subdir
+#TODO: copy strings here and dynamically create, set scan_dir properly
 sample_tsai=$(realpath $2)
+#Reference DEM
 refdem=$(realpath $3)
 outfn=$4
-#can accept corner coordinates and corner pixel locations as input, for now they are hardcoded
 
-# mosaic the images using interest point matching
+#For now, assume only one image ID per directory
+#Assume filenames are of the form D3C1217-200742A004_a.tif
+img_list=$(ls $img_dir/*_[a-z].tif)
+
+#Combine input images using interest point matches to create full-resolutoin mosaic
 mos_opt=""
 mos_opt+=" --ot Byte --overlap-width 3000"
 pre_out=${outfn%.*}_pre_mos.tif
-echo "mosaicing image"
 eval image_mosaic $img_list $mos_opt -o $pre_out
 
-# hardcoded corner pixel coordinates for cropping
-# can be accepted as input, or calculated by a script
+#Remove frame from mosaicked image
+#Can accept corner coordinates and corner pixel locations as input, for now they are hardcoded
+#TODO: create tool to identify frame corners 
+#TODO: revisit rotation and corner coordinate order for different scan directions 
 px_cor='980 1663 116553 1626 116497 23435 1038 23461'
-crop_op=""
-crop_op+=" rotate-crop"
-# Cropping image from frame
-echo "cropping image from frame"
-eval historical_helper.py crop_op --input-path $pre_out --output-path $outfn --interest-points $px_cor
+eval historical_helper.py rotate-crop --input-path $pre_out --output-path $outfn --interest-points $px_cor
 
-#hardcoded corner coordinate provided by USGS
-init_camera=${outfn%.*}_init_obc.tsai
+#Extract corner coordinates from USGS polygon geometry
+#Hardcoded for now
 cor_coord='-123.335 46.407 -122.031 46.278 -122.1 46.1 -123.368 46.25'
-cam_gen_opt=""
-cam_gen_opt+=" --camera-type opticalbar --refine-camera"
-echo "generating initial optical bar camera"
-eval cam_gen $cam_gen_opt --sample-file $sample_tsai --lon-lat-values $cor_coord --reference-dem $refdem $outfn -o $init_camera
-echo "Script is complete !"
+
+#Generate initial camera model for cropped full-resolution mosaic
+init_camera=${outfn%.*}_init_obc.tsai
+eval cam_gen --camera-type opticalbar --refine-camera --sample-file $sample_tsai --lon-lat-values $cor_coord --reference-dem $refdem $outfn -o $init_camera
--- scripts/proc_obc.sh
+++ scripts/proc_obc.sh
@@ -1,68 +1,97 @@
 #! /bin/bash
+
+#Wrapper shell script for sample Hexagon (KH-9) stereo processing workflow 
+#Shashank Bhushan, David Shean
+
+#Requires command-line utilities from the NASA Ames Stereo Pipeline
+
+#Input images and camera models (output from preprocessing script)
 img1=$(realpath $1)
 img2=$(realpath $2)
 cam1=$(realpath $3)
 cam2=$(realpath $4)
+
+#Reference DEM
 refdem=$(realpath $5)
 outdir=$(realpath $6)
+
+#EPSG code for output projection should be of form "EPSG:32610"
+#TODO: extract from reference DEM
 epsg=$7
 
-echo "Input images and cameras are "$img1 $img2 $cam1 $cam2
-echo "Reference DEM is"$refdem
+#Coarse DEM resolution for intermediate steps
+coarse_tr=120
+#Fine DEM grid resolutoin for final products
+fine_tr=4
 
-# Running initial stereo
+echo "Input images and cameras: $img1 $img2 $cam1 $cam2"
+echo "Reference DEM: $refdem"
+
+# Run stereo with initial camera models
 init_stereo_dir=$outdir/init_stereo
 stereo $img1 $img2 $cam1 $cam2 --alignment-method affineepipolar --unalign-disparity $init_stereo_dir/run
-#use point clound and unalign disparity map 
+
+#Identify output point clound (PC) and unalign disparity map 
 pc_init=$(realpath $init_stereo_dir/*-PC.tif)
 un_disp_1=$(realpath $init_stereo_dir/*unaligned-D.tif)
-#grid point cloud to coarse DEM, here at 120 m
-point2dem $pc_init --t_srs $epsg --tr 120
+
+#Create coarse gridded DEM from point cloud
+point2dem $pc_init --t_srs $epsg --tr $coarse_tr
 dem_1=$(realpath $init_stereo_dir/*-DEM.tif)
-#align coarse DEM to reference DEM
+
+#Align coarse DEM to reference DEM
 pc_align $refdem $dem_1 --max-displacement -1 --initial-transform-from-hillshading similarity --save-transformed-source-points -o $init_stereo_dir/run
 # the DEM/point cloud alignment are saved in the DEM output folder
 transform_1=$(realpath $init_stereo_dir/*-transform.txt)
-echo "Round 1 stereo and co-registration complete, now optimizing extrinsics"
+
+# Update camera model extrinsics
 ba_dir_1=$outdir/ba_extrinsic
 # need to migrate these calls to "eval" structure
-# use reference terrain as default, other options include to use heights from DEM and both the options in combo, provided below but commented out
-echo "refining extrinsic"
+# Use reference DEM 
 bundle_adjust $img1 $img2 $cam1 $cam2 --max-iterations 50 --camera-weight 0 --disable-tri-ip-filter --inline-adjustments --ip-detect-method 1 -t opticalbar --datum WGS84 --force-reuse-match-files --reference-terrain-weight 1000 --parameter-tolerance 1e-12 --max-disp-error 100 --disparity-list $un_disp_1 --reference-terrain $refdem --initial-transform $transform_1 -o $ba_dir_1/run
-#heights from DEM
+# Use heights from reference DEM
 #bundle_adjust $img1 $img2 $cam1 $cam2 --max-iterations 50 --camera-weight 0 --disable-tri-ip-filter --inline-adjustments --ip-detect-method 1 -t opticalbar --datum WGS84 --force-reuse-match-files --heights-from-dem-weight 1000 --parameter-tolerance 1e-12 --max-disp-error 100 --heights-from-dem $refdem --initial-transform $transform_1 -o $ba_dir_1/run
+# Use both reference DEM and heights from reference DEM
 #bundle_adjust $img1 $img2 $cam1 $cam2 --max-iterations 50 --camera-weight 0 --disable-tri-ip-filter --inline-adjustments --ip-detect-method 1 -t opticalbar --datum WGS84 --force-reuse-match-files --heights-from-dem-weight 1000 --parameter-tolerance 1e-12 --max-disp-error 100 --heights-from-dem $refdem --disparity-list $un_disp_1 --reference-terrain $refdem --initial-transform $transform_1 -o $ba_dir_1/run
-# Need to check some of the hard codes here, result can be potentially better
-# Use the updated cameras to do stereo
+
+# Run second round of stereo with updated camera models
 cam1_ex=$ba_dir_1/run-*$cam1
 cam2_ex=$ba_dir_1/run-*$cam2
-stereo_dir_2=$outdir/stereo_ex
-stereo $img1 $img2 $cam1_ex $cam2_ex --alignment-method affineepipolar --unalign-disparity $stereo_dir_2/run 
+stereo_dir_2=$outdir/stereo_extrinsic
 # using affineepipolar might not make sense here, is there an epipolar line existing in first place ?
+stereo $img1 $img2 $cam1_ex $cam2_ex --alignment-method affineepipolar --unalign-disparity $stereo_dir_2/run 
+
+#Create updated coarse gridded DEM
 pc_ex=$(realpath $stereo_dir2/*-PC.tif)
 un_disp_2=$(realpath $stereo_dir2/*unaligned-D.tif)
-point2dem $pc_ex --t_srs $epsg --tr 120
+point2dem $pc_ex --t_srs $epsg --tr $coarse_tr 
 dem_2=$(realpath $stereo_dir_2/*-DEM.tif)
-#align coarse DEM to reference DEM
+
+#Align updated coarse DEM to reference DEM
 pc_align $refdem $dem_2 --max-displacement -1 --initial-transform-from-hillshading similarity --save-transformed-source-points -o $stereo_dir_2/run
 transform_2=$(realpath $stereo_dir_2/*-transform.txt)
-# Optimise for intrinsics
+
+#Optimise for camera model intrinsics
 $ba_dir_2=$outdir/ba_intrinsic
-echo "refining intrinsic"
 bundle_adjust $img1 $img2 $cam1_ex $cam2_ex -t opticalbar --force-reuse-match-files --max-iterations 30 --camera-weight 0 --disable-tri-ip-filter --inline-adjustments --ip-detect-method 1 --datum WGS84 --num-passes 2 --solve-intrinsics --intrinsics-to-float "focal_length optical_center other_intrinsics" --intrinsics-to-share "focal_length optical_center" --ip-per-tile 1000 --intrinsics-limits "0.95 1.05 0.90 1.10 0.90 1.10 0.5 1.5 -5.0 5.0 0.3 2.0" --num-random-passes 2 --max-disp-error 100 --disparity-list $un_disp_2 --reference-terrain $refdem --initial-transform $transform_2 --reference-terrain-weight 1000 -o $ba_dir_2/run
-# Load updated cameras as variables for stereo, orthorectification
+
+#Option - do an additional round of extrinsic/intrinsic refinement
+
+#Run final round of stereo
 cam1_in=$ba_dir_2/run-*$cam1_ex
 cam2_in=$ba_dir_2/run-*$cam2_ex
 stereo_dir_3=$outdir/stereo_in
-echo "performing final stereo"
 stereo $img1 $img2 $cam1_in $cam2_in --alignment-method affineepipolar --unalign-disparity $stereo_dir_3/run
 pc_in=$(realpath $stereo_dir3/*-PC.tif)
-point2dem $pc_in --t_srs $epsg --tr 4
+
+#Create final gridded DEM at fine resolution
+point2dem $pc_in --t_srs $epsg --tr $fine_tr
 dem_fin=$(realpath $stereo_dir3/*-DEM.tif)
-# produce ortho image from left image
+
+#Create ortho image from left image
+#TODO: create orthoimages from both inputs, use original filenames, not "left/right"
+#ortho=$outdir/${img1%.*}_ortho.tif
 ortho=$outdir/left_ortho.tif
-dem=$refdem
-#dem=$dem_fin
-echo "generating ortho image"
-mapproject -t opticalbar $dem --t_srs $epsg $img1 $cam1_in $ortho
-echo "script is complete !"
+ortho_dem=$refdem
+#ortho_dem=$dem_fin
+mapproject -t opticalbar $ortho_dem --t_srs $epsg $img1 $cam1_in $ortho
